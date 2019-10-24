6aaccaab360bc366bd05822b1e66a31a75fb27f1
Marc Pabst <mapabst@roxette.cbs.mpg.de>
2019-10-24 18:22:41 +0200

    Updated dependencies...

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
      name='dvasl',  
      version='0.1',
@@ -10,5 +13,6 @@ setup(
      author_email="mapabst@cbs.mpg.de",
      packages=find_packages(),
      description="Searchlight-like analysis for fMRI data.",
+     install_requires=requirements,
      long_description=long_description
  )
